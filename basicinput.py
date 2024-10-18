3137d7d1be6c899603551f7fa4366700c916fc9b
Adithyas0710 <adithyas1064@gmail.com>
2024-10-18 14:31:36 +0530

    Add files via upload

---
+++ basicinput.py
@@ -0,0 +1,11 @@
+'''
+Author:Adithya S
+Date:18-10-2024
+Description:program that asks the user to input
+ their name and age , After receiving the input,
+ display a message that includes the user's name and age.
+ Hello, Adithya! You are 18 years old.
+'''
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Hello,",name+"! you are",age," years old")
