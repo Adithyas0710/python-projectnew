c38f66b7a2f3dbb8528a93620aa55c81d3b6ec73
Adithyas0710 <adithyas1064@gmail.com>
2024-10-18 15:12:21 +0530

    Add files via upload

---
+++ stringnew.py
@@ -0,0 +1,15 @@
+'''
+Author:Adithya
+Date:18-10-2024
+Description:Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+and then concatenate it with another string. Finally, display the new string.
+'''
+
+
+
+
+str_var = "Hello, World!"
+str_another_var = str_var[7:12]
+print(str_another_var)
+str_var2 = " Everyone!"
+print(str_another_var + str_var2)
