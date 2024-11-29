5170f94a2378584f957ed3f20fc3b71c109b5c43
Adithyas0710 <adithyas1064@gmail.com>
2024-11-29 14:34:39 +0530

    Add files via upload

---
+++ num_.py
@@ -0,0 +1,14 @@
+'''
+Author : Adithya S
+Date : 29-11-2024
+Description : Program to check whether the given number is a valid mobile number or not using functions.
+'''
+def is_valid_mobile_number(number):
+    if len(number)==10 and number[0] in "987":
+        return True
+    return False
+mobile_number=input("enter your mobile number:")
+if is_valid_mobile_number(mobile_number):
+    print(f"The mobile number is {mobile_number} is valid")
+else:
+    print(f"The mobile number is {mobile_number} is invalid")
