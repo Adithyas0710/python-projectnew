2a1d545af13d3f70b6aa19141bc7724ae7670395
Adithyas0710 <adithyas1064@gmail.com>
2024-11-30 15:27:48 +0530

    Add files via upload

---
+++ max_.py
@@ -0,0 +1,11 @@
+def max_of_three(number_list):
+    m=max(number_list)
+    return m
+number_list = []
+for i in range(3):
+    num1=int(input("Enter a number:"))
+    number_list.append(num1)
+max_of_three(number_list)
+print(max_of_three(number_list))
+
+
