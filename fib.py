4599ad173553556a7ac78a3199c0eb6fdfa6f426
Adithyas0710 <adithyas1064@gmail.com>
2024-12-06 16:27:18 +0530

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,3 @@
+import fibino_num as fib
+N=int(input("Enter the no. of numbers you need:"))
+fib.fibonacci_1(N)
---
+++ fibino_num.py
@@ -0,0 +1,11 @@
+def fibonacci_1(limit):
+    num1=0
+    num2=1
+    print("Fibonacci series")
+    print(num1)
+    print(num2)
+    for i in range (limit):
+        sum=num1+num2
+        print(sum)
+        num1=num2
+        num2=sum
